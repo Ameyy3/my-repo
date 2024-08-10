cc5f74d0f52d4a2f31266745b54eaf4b445de20b
Amey <ameyshinde3114@gmail.com>
2024-08-10 15:14:17 +0530

    2nd commit

--- Api.js
+++ Api.js
@@ -24,39 +24,39 @@ app.post("/compile", function (req, res) {
     const code = req.body.code;
     const input = req.body.input;
     const lang = req.body.lang;
+
+    const startTime = Date.now(); // Start time
+
+    const sendResponse = (data) => {
+        const endTime = Date.now(); // End time
+        const executionTime = endTime - startTime;
+        res.send({
+            output: data.output ? data.output : "error",
+            executionTime: executionTime + " ms"
+        });
+    };
+
     try {
         if (lang === "Cpp") {
             const envData = { OS: "windows", cmd: "g++", options: { timeout: 10000 } };
             if (!input) {
-                compiler.compileCPP(envData, code, function (data) {
-                    res.send(data.output ? data : { output: "error" });
-                });
+                compiler.compileCPP(envData, code, sendResponse);
             } else {
-                compiler.compileCPPWithInput(envData, code, input, function (data) {
-                    res.send(data.output ? data : { output: "error" });
-                });
+                compiler.compileCPPWithInput(envData, code, input, sendResponse);
             }
         } else if (lang === "Java") {
             const envData = { OS: "windows" };
             if (!input) {
-                compiler.compileJava(envData, code, function (data) {
-                    res.send(data.output ? data : { output: "error" });
-                });
+                compiler.compileJava(envData, code, sendResponse);
             } else {
-                compiler.compileJavaWithInput(envData, code, input, function (data) {
-                    res.send(data.output ? data : { output: "error" });
-                });
+                compiler.compileJavaWithInput(envData, code, input, sendResponse);
             }
         } else if (lang === "Python") {
             const envData = { OS: "windows" };
             if (!input) {
-                compiler.compilePython(envData, code, function (data) {
-                    res.send(data.output ? data : { output: "error" });
-                });
+                compiler.compilePython(envData, code, sendResponse);
             } else {
-                compiler.compilePythonWithInput(envData, code, input, function (data) {
-                    res.send(data.output ? data : { output: "error" });
-                });
+                compiler.compilePythonWithInput(envData, code, input, sendResponse);
             }
         }
     } catch (e) {
